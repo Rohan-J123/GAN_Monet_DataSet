abca6a5bbe94c8a0ec3ff5d3768c3097fdf3fc19
Rohan-J123 <rohanjha@aol.in>
2024-11-18 14:42:58 +0530

    New Data Generated

--- pytorch_fid/__main__.py
+++ pytorch_fid/__main__.py
@@ -1,5 +1,9 @@
 if __name__ == "__main__":
     import fid_score
-    s = ["/content/GAN_Monet_DataSet/generated_jpg_1", "/content/GAN_Monet_DataSet/generated_jpg_2", "/content/GAN_Monet_DataSet/generated_jpg_3", "/content/GAN_Monet_DataSet/generated_jpg_4", "/content/GAN_Monet_DataSet/generated_jpg_5", "/content/GAN_Monet_DataSet/generated_jpg_6", "/content/GAN_Monet_DataSet/generated_jpg_7", "/content/GAN_Monet_DataSet/generated_jpg_8"]
+    s = ["/content/GAN_Monet_DataSet/generated_jpg_1", "/content/GAN_Monet_DataSet/generated_jpg_2", 
+         "/content/GAN_Monet_DataSet/generated_jpg_3", "/content/GAN_Monet_DataSet/generated_jpg_4", 
+         "/content/GAN_Monet_DataSet/generated_jpg_5", "/content/GAN_Monet_DataSet/generated_jpg_6", 
+         "/content/GAN_Monet_DataSet/generated_jpg_7", "/content/GAN_Monet_DataSet/generated_jpg_8",
+         "/content/GAN_Monet_DataSet/generated_jpg_9"]
     for one_s in s:
         fid_score.main(one_s)
